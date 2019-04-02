6f4b9ac05819078dc34332b26ff8a44299a997cf
STIVEN MORVAN <stiven.morvan@gmail.com>
2019-04-02 20:00:45 +0200

    Presentation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# nifty_dataset_resizer
+# Nifty dataset resizer
+
+A simple function for resizing a Nifty dataset.
+
+## Usage
+
+Just give the folder path and it will create a copy folder with the resized images.
+
+- resizeNiftyFolder('PathToImages/')
+
+- resizeNiftyFolder('PathToLabel/', inter=cv2.INTER_NEAREST)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 import nibabel as nib
 
-def resizeAndSaveNifti(path, size, inter):
+def resizeAndSaveNifty(path, size, inter):
     size = 128
     new_size_y = 128
     initial_image = nib.load(path).get_data()
@@ -16,13 +16,9 @@ def resizeAndSaveNifti(path, size, inter):
 
     return nib.Nifti1Image(new_data, np.eye(4))
 
-def resizeNiftiFolder(folder, size=128, inter=cv2.INTER_CUBIC):
+def resizeNiftyFolder(folder, size=128, inter=cv2.INTER_CUBIC):
     if not os.path.exists('resized/'+folder): os.makedirs('resized/'+folder)
     for filename in os.listdir(folder):
-        img = resizeAndSaveNifti(folder+filename, size, inter)
+        img = resizeAndSaveNifty(folder+filename, size, inter)
         print(folder+filename);
         img.to_filename('resized/'+folder+filename)
-
-#resizeNiftiFolder('Task03_Liver/imagesTr/')
-
-resizeNiftiFolder('Task03_Liver/labelsTr/', inter=cv2.INTER_NEAREST)
