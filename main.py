52f31cf0ba201e6d2003f8c8feb57761539a82b1
STIVEN MORVAN <stiven.morvan@gmail.com>
2019-04-02 19:55:06 +0200

    Adaptation for the labels

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ import cv2
 import numpy as np
 import nibabel as nib
 
-def resizeAndSaveNifti(path, size=128):
+def resizeAndSaveNifti(path, size, inter):
     size = 128
     new_size_y = 128
     initial_image = nib.load(path).get_data()
     slices = initial_image.shape[2]
     new_data = np.zeros((size,new_size_y,slices))
-    print(initial_image.shape)
 
     for i in range(0,slices):
-        temp_slice = cv2.resize(initial_image[..., i], (size, new_size_y), interpolation=cv2.INTER_CUBIC)
+        temp_slice = cv2.resize(initial_image[..., i], (size, new_size_y), interpolation=inter)
         new_data[...,i] = temp_slice
 
     return nib.Nifti1Image(new_data, np.eye(4))
 
-def resizeNiftiFolder(folder):
+def resizeNiftiFolder(folder, size=128, inter=cv2.INTER_CUBIC):
     if not os.path.exists('resized/'+folder): os.makedirs('resized/'+folder)
     for filename in os.listdir(folder):
-        img = resizeAndSave(folder+filename)
+        img = resizeAndSaveNifti(folder+filename, size, inter)
         print(folder+filename);
         img.to_filename('resized/'+folder+filename)
 
-resizeNiftiFolder('Task03_Liver/imagesTr/')
+#resizeNiftiFolder('Task03_Liver/imagesTr/')
+
+resizeNiftiFolder('Task03_Liver/labelsTr/', inter=cv2.INTER_NEAREST)
